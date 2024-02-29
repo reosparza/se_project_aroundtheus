909ac492fdb04e6073ab317912f380455f20ba61
resparza <rafael.esparza45@gmail.com>
2024-02-29 13:29:32 -0500

    add initial validation script

---
+++ scripts/validate.js
@@ -0,0 +1,45 @@
+// enabling validation by calling enableValidation()
+// pass all the settings on call
+
+function setEventListeners(formEl, options) {
+  const { inputSelector } = options;
+  const inputEls = [...formEl.querySelectorAll(inputSelector)];
+  inputEls.forEach((inputEl) => {
+    inputEl.addEventListener("input", (e) => {
+      console.log(inputEl.validity);
+    });
+  });
+}
+
+function enableValidation(options) {
+  const formEls = [...document.querySelectorAll(options.formSelector)];
+  formEls.forEach((formEl) => {
+    formEl.addEventListener("submit", (e) => {
+      e.preventDefault(); //can possibly remove preventdefault on index.js
+    });
+
+    setEventListeners(formEl, options);
+
+    // look for all inputs inside of form
+    // loop through all the inputs to see if all are valid
+    // if input is not valid
+    // get validation message
+    // add error class to input
+    // display error message
+    // disable buton
+    // if all inputs are valid
+    // enable button
+    // reset error messages
+  });
+}
+
+const config = {
+  formSelector: ".modal__form",
+  inputSelector: ".modal__input",
+  submitButtonSelector: ".modal__save-button",
+  inactiveButtonClass: "popup__button_disabled",
+  inputErrorClass: "modal__input_type_error",
+  errorClass: "popup__error_visible",
+};
+
+enableValidation(config);
