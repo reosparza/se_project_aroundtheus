6d18d9a2de7a1a741b8204a3f40687ec60733fc8
resparza <rafael.esparza45@gmail.com>
2024-04-04 20:11:39 -0400

    Initial setup of popup and section javascript classes

---
+++ components/Popup.js
@@ -0,0 +1,13 @@
+export default class Popup {
+  constructor(popupSelector) {
+    this.popupSelector = popupSelector;
+  }
+
+  open() {}
+
+  close() {}
+
+  _handleEscClose() {}
+
+  _setEventListeners() {}
+}
---
+++ components/Section.js
@@ -0,0 +1,3 @@
+export default class Section {
+  constructor({ items, renderer }, selector) {}
+}
